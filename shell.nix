542db12ff587f22de549d96963db127b3d1000d5
chimnayajith <chinmayajith30@gmail.com>
2026-03-15 17:35:48 +0530

    feat: Update shell.nix for ml module

--- shell.nix
+++ shell.nix
@@ -4,9 +4,14 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs_20
     nodePackages.npm
+    python311
+    python311Packages.pip
+    cudatoolkit
+    stdenv.cc.cc
   ];
 
   shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
     echo "Node.js development environment loaded"
     echo "Node version: $(node --version)"
     echo "NPM version: $(npm --version)"
